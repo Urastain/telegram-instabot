50503db585e4e877df224a2a6d2c09b12c0ae181
Urastain <urastain@gmail.com>
2025-06-09 15:05:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,33 +12,32 @@ BOT_URL = os.getenv("BOT_URL")
 app = Flask(__name__)
 bot = Bot(token=TOKEN)
 
-# Telegram Application (аналог Dispatcher)
+# Telegram Application
 application = Application.builder().token(TOKEN).build()
 
 # Обработчик команды /start
 async def start(update: Update, context):
     await update.message.reply_text("Привет! Пришли ссылку на Instagram-видео.")
 
-# Обработка входящих обновлений
+# Устанавливаем webhook
+async def set_webhook():
+    url = f"{BOT_URL}/{TOKEN}"
+    await bot.set_webhook(url)
+    await application.initialize()  # <--- ВАЖНО
+    print("Webhook установлен и приложение инициализировано")
+
+# Flask route для Telegram webhook
 @app.route(f"/{TOKEN}", methods=["POST"])
 def webhook():
     update = Update.de_json(request.get_json(force=True), bot)
     asyncio.run(application.process_update(update))
     return "OK", 200
 
-# Устанавливаем webhook
-async def set_webhook():
-    url = f"{BOT_URL}/{TOKEN}"
-    await bot.set_webhook(url)
-
 # Запуск сервера
 if __name__ == "__main__":
-    # Регистрируем handlers
     application.add_handler(CommandHandler("start", start))
 
-    # Установка webhook
     loop = asyncio.get_event_loop()
     loop.run_until_complete(set_webhook())
 
-    # Запускаем Flask
     app.run(host="0.0.0.0", port=10000)
